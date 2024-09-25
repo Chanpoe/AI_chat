02e32c1f4e0f6aae39249db704bd6f02f63af779
Chanpoe's WorkBook <941407617@qq.com>
2024-09-25 10:43:31 +0800

    update 更新上下文对象的类定义，增加基类Context
    其他模型对应的上下文类都可以继承于基类，新增LlamaContext类继承于OpenAIContext

--- util/context.py
+++ util/context.py
@@ -3,12 +3,40 @@
 # Date ：2024/09/24 10:04
 # IDE：PyCharm
 
-class OpenAIContext:
+from azure.ai.inference.models import SystemMessage, UserMessage, AssistantMessage
+from typing import List, Any
+
+
+# 上下文基类
+class Context:
     def __init__(self, character: str = "default"):
         # 角色身份
         self.character = character
         # 历史消息
-        self.historical_messages = None
+        self.historical_messages: List[Any] = []
+        # 设置默认历史消息
+        self.set_default_history()
+
+    # 重置对话消息，将对话历史记录设置为角色身份对应的system prompt
+    def set_default_history(self):
+        pass
+
+    # 添加一条用户发送的消息
+    def add_user_message(self, content: str):
+        pass
+
+    # 添加一条AI回复的消息
+    def add_assistant_message(self, content: str):
+        pass
+
+    # 清除对话历史记录
+    def clear_history(self):
+        self.set_default_history()
+
+
+class OpenAIContext(Context):
+    def __init__(self, character: str = "default"):
+        super().__init__(character=character)
         # 设置默认历史消息
         self.set_default_history()
 
@@ -18,7 +46,7 @@ class OpenAIContext:
             self.historical_messages = [
                 {
                     "role": "system",
-                    "content": "末尾附带不包含🤖的emoji表情，并且使用尽可能短但是意思表述完整的中文回答，如果生成的内容包含markdown格式，请在回复的开头第一行加上'markdown=true'",
+                    "content": "末尾附带不包含🤖的emoji表情，并且使用尽可能短但是意思表述完整的中文回答，回答的风格幽默一些。如果生成的内容包含markdown格式，请在回复的开头第一行加上'markdown=true'",
                 }
             ]
         # todo: 添加其他角色身份
@@ -37,5 +65,15 @@ class OpenAIContext:
             "content": content,
         })
 
-    def clear_history(self):
+
+# Llama对话上下文与OpenAI对话上下文相同，故继承OpenAIContext
+class LlamaContext(OpenAIContext):
+    def __init__(self, character: str = "default"):
+        super().__init__(character=character)
         self.set_default_history()
+
+
+if __name__ == '__main__':
+    context = LlamaContext()
+    print(context.historical_messages)
+    print(context.character)
